41898890fd75c826a1292e505a13845d194934d3
saurabh nikumbh <saurabh5nikumbh@gmail.com>
2026-01-23 00:36:04 +0530

    add cpu usage monitoring script

---
+++ cpu_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+THRESHOLD=10
+
+#CPU idel % kadhto (vmstat)
+IDLE=$(vmstat 1 2 | tail -1 | awk '{print $15}')
+
+#Used = 100 - idle
+USAGE=$((100 - IDLE))
+
+echo "Current CPU Usage: ${USAGE}%"
+
+if [ "$USAGE" -ge "$THRESHOLD" ]; then
+    echo "WARNING: CPU USAGE AOVE ${THRESHOLD}%"
+    exit 1
+else
+    echo "OK: CPU USAGE UNDER CONTROL"
+    exit 0
+fi
